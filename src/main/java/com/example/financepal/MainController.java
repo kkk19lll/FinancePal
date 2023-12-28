7432111933115c96c99e41d8dcd9c8f519d6d7ce
kkk19lll <kirillkurnakin@mail.ru>
2023-12-28 10:45:37 +0300

    Main Commit2

--- src/main/java/com/example/financepal/MainController.java
+++ src/main/java/com/example/financepal/MainController.java
@@ -1,5 +1,79 @@
 package com.example.financepal;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
 public class MainController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ImageView addWallet;
+
+    @FXML
+    private ImageView deleteWallet;
+
+    @FXML
+    private TableColumn<?, ?> descriptionInvestmentsTableColumn;
+
+    @FXML
+    private TableColumn<?, ?> idInvestmentsTableColumn;
+
+    @FXML
+    private TableColumn<?, ?> idWalletTableColumn;
+
+    @FXML
+    private AnchorPane investmentsAnchorPane;
+
+    @FXML
+    private AnchorPane mainAnchorPane;
+
+    @FXML
+    private TableColumn<?, ?> moneyWalletTableColumn;
+
+    @FXML
+    private TableColumn<?, ?> nameInvestmentsTableColumn;
+
+    @FXML
+    private TableColumn<?, ?> nameWalletTableColumn;
+
+    @FXML
+    private PieChart pieChartStatistics;
+
+    @FXML
+    private TableColumn<?, ?> priceIvestmentsTableColumn;
+
+    @FXML
+    private AnchorPane statisticsAnchorPane;
+
+    @FXML
+    private TableView<?> tableViewInvetsments;
+
+    @FXML
+    private TableView<?> tableViewWallet;
+
+    @FXML
+    private ImageView updateWallet;
+
+    @FXML
+    private TableColumn<?, ?> userWalletTableColumn;
+
+    @FXML
+    private AnchorPane walletAnchorPane;
+
+    @FXML
+    void initialize() {
+
+    }
 
 }
--- src/main/java/com/example/financepal/db/DbFunctions.java
+++ src/main/java/com/example/financepal/db/DbFunctions.java
@@ -68,4 +68,6 @@ public class DbFunctions {
         }
         return 201;
     }
+
+
 }
